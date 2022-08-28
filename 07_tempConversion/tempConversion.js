201d3dea9700214d56c0484c3bf2495007d82e17
sblaize <blaizesekani@gmail.com>
2022-08-27 23:38:56 -0400

    Created function to convert negative or positive temp to either Celsius or Fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,22 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+let fMinus = f-32;
+let top = fMinus * 5;
+let c = top/9
+if (c > 0 || c < 0) {
+  return parseFloat(c.toFixed(1))
+  } else {
+    return 0
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let cMultiply = c*((9/5));
+  let f = cMultiply + 32
+  if (f > 0 || f < 0) {
+  return parseFloat(f.toFixed(1))
+  } else {
+    return 0
+  }
 };
 
 // Do not edit below this line
