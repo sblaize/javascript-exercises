b2871fe61e5876e64bea8a2cbe77ed060782a000
sblaize <blaizesekani@gmail.com>
2022-08-27 23:32:34 -0400

    Created function to repeat string regardless of condition

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+if (num>0) {
+    return string.repeat(num)
+}
+else if (num === 0) {
+    return ""
+}
+else if (num < 0) {
+    return "ERROR"
+}
+else if (num = "") {
+    const number = Math.floor(Math.random() * 1000)
+    return string.repeat(number)
+}
+else if (string = "") {
+    return ""
+}
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
