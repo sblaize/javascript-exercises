822880b1a5e540efbbedefd45107af43a3b43a0c
sblaize <blaizesekani@gmail.com>
2022-08-27 23:36:34 -0400

    Created function to check for which argument is larger and find the sum between it and the smaller number.Also if either number is negative or not a number returns ERROR

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+let biggerNum;
+let smallerNum;
+let finalSum;
+if (num1>num2 && typeof num1 === "number" && typeof num2 === "number" && num1 > 0 && num2 > 0) {
 
+const biggerNum = parseInt(num1);
+const smallerNum = parseInt(num2);
+const finalSum = (biggerNum/2) * (biggerNum + smallerNum)
+return finalSum
+
+} else if (num1<num2 && typeof num1 === "number" && typeof num2 === "number" && num1 > 0 && num2 > 0){
+
+const biggerNum = parseInt(num2);
+const smallerNum = parseInt(num1);
+const first = biggerNum/2;
+const second = biggerNum + smallerNum;
+const finalSum = first * second
+return finalSum
+}
+else if (num1 < 0 || num2 < 0) {
+    return "ERROR"
+} else if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return "ERROR"
+}
 };
 
 // Do not edit below this line
