afbfe688591d8ff39bd3128d5e3c52323cfa1f74
sblaize <blaizesekani@gmail.com>
2022-08-27 23:35:05 -0400

    Created function to take argument and remove it from the array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, arg, arg2, arg3, arg4) {
+    
+    return arr.filter(function(ele){
+        return ele !== arg && ele !== arg2 && ele !== arg3 && ele !== arg4;
+    });
+    {
+        arr2 === arr.filter()
+            return arr2
+        }
 };
 
 // Do not edit below this line
