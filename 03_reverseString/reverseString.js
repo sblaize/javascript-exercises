9bd673d3de9eba1cf22bf03763d77692990bb7ae
sblaize <blaizesekani@gmail.com>
2022-08-27 23:33:56 -0400

    Created function to reverse and join string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+if (string) {
+const rs = string.split('');
+const rsflip = rs.reverse()
+return   rsflip.join("");
+}
+else {
+    return ""
+}
 };
 
 // Do not edit below this line
