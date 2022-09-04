16f301abb472a4a211637a059773c61e4d784153
sblaize <blaizesekani@gmail.com>
2022-09-03 20:45:06 -0400

    Practiced creating and appending elements

---
+++ js_practice/script.js
@@ -0,0 +1,27 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('p');
+content.classList.add('content');
+content.textContent = "Hey I'm red!";
+content.style.cssText = "color: red;";
+
+container.appendChild(content);
+
+const heading3 = document.createElement('h3');
+heading3.classList.add('heading3');
+heading3.textContent = "I'm a blue h3!";
+heading3.style.cssText = "color: blue;";
+
+container.appendChild(heading3);
+
+const div = document.createElement('div');
+const head1 = document.createElement('h1');
+const paragraph = document.createElement('p');
+div.classList.add('vid');
+head1.textContent = "I'm in a div";
+paragraph.textContent = "ME TOO!";
+div.style.cssText = "border: black; background-color: pink;";
+div.appendChild(head1);
+div.appendChild(paragraph);
+
+container.appendChild(div);
