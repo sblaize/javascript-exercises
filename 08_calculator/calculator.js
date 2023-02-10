924220782b38d6ff38e3e6fc5c98fe7b4cd40dab
sblaize <blaizesekani@gmail.com>
2023-02-10 18:34:06 -0500

    Created functions for each instance asked

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(x, y) {
+let adder = x + y
+return adder
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+if (x > y) {
+  x = x
+} else if (x < y) {
+x = y
+}
+let minus = x - y
+return minus
 };
 
-const sum = function() {
-	
+const sum = function(n) {
+  let amount = n.reduce((a,b) => {
+  return a + b;
+}, 0)
+return amount
 };
 
-const multiply = function() {
-
+const multiply = function(n) {
+  let amount = n.reduce((a,b) => {
+    return a * b;
+  }, 1)
+  return amount
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+return x**y
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } 
+  for (let i = n-1; i > 0; i--) {
+  n *= i;
+}
+return n
 };
 
 // Do not edit below this line
