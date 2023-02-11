a7fab5be390a4801e25f95a7fe8e8a0fd8cc46dd
sblaize <blaizesekani@gmail.com>
2023-02-11 08:58:44 -0500

    Created function to find titles of books

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+const findTitle = obj.map(book => (`${book.title}`))
+return findTitle
 };
 
 // Do not edit below this line
