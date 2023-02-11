b9cee8a02f5ed8cdbb7631a793ac55ee96fb4714
sblaize <blaizesekani@gmail.com>
2023-02-11 08:45:55 -0500

    Created fibonacci function for positive numbers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let Phi = (Math.sqrt(5) + 1)/(2)
+    let phi =(Phi - 1)
+    let fib = (Phi**n - (-phi)**n)/(Math.sqrt(5))
+    if (n < 0){
+        return 'OOPS'
+    } else {
+        return Math.abs(parseInt(fib));
+}};
 
-};
 
 // Do not edit below this line
 module.exports = fibonacci;
