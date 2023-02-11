479471d33daf74057397b6759db6f332dae100b8
sblaize <blaizesekani@gmail.com>
2023-02-11 11:27:28 -0500

    Created function to find the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function (people) {
+    const age = (x) => (x.yearOfDeath || new Date().getFullYear()) - x.yearOfBirth;
+    const oldest = people.sort((a, b) =>
+      age(a) > age(b) ? -1 : 1
+    );
+    return oldest[0];
+  };
+  
 // Do not edit below this line
 module.exports = findTheOldest;
