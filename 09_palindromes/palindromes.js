240a7ac35260d0528f18a6bdc4094cb8ce06cd20
sblaize <blaizesekani@gmail.com>
2023-02-10 19:42:26 -0500

    Created function to check if string is palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+    var re = /[\W_]/g;
+    var orgStr = str.toLowerCase().replace(re, '');
+    var reverseStr = orgStr.split('').reverse().join(''); 
+    return reverseStr === orgStr;
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
